386db703f3477b671cc407bf24ced39fb7ff136e
GUNAVARDHANSAI P <96119286+Gunavardhansai@users.noreply.github.com>
2023-09-16 16:54:06 +0530

    Create models.py

---
+++ property_scraper/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Property(models.Model):
+    property_name = models.CharField(max_length=255)
+    property_cost = models.CharField(max_length=50)
+    property_type = models.CharField(max_length=100)
+    property_area = models.CharField(max_length=50)
+    property_locality = models.CharField(max_length=255)
+    property_city = models.CharField(max_length=255)
+    property_link = models.URLField()
+
+    def __str__(self):
+        return self.property_name
+#property_scraper
+import requests
+from bs4 import BeautifulSoup
+from django.core.management.base import BaseCommand
+from property_scraper.models import Property
+
+class Command(BaseCommand):
+    help = 'Scrape property data from 99acres and store it in MongoDB'
+
+    def handle(self, *args, **kwargs):
+        # Your scraping logic here
+        pass
